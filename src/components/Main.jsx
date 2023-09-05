46a96718020736a518261ac04e930e1633d7ee75
Pratap Khandekar <90126305+clashhulk@users.noreply.github.com>
2023-09-05 12:43:21 +0530

    Update Main.jsx
    file name case change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "./styles/main.module.css";
+import styles from "./styles/Main.module.css";
 
 const Main = ({ children }) => {
   return <main className={styles.pageContainer}>{children}</main>;
